81957ff8aeb08a68ffb9f54e11036f957190805a
Taufik Nurrahman <official.taufik980@gmail.com>
2023-08-30 15:53:38 +0700

    view pdf

--- src/main/components/PageErrorMessage.js
+++ src/main/components/PageErrorMessage.js
@@ -2,25 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function PageErrorMessage({
-  title = "404 NOT FOUND",
-  body = "Looks like this page not found",
+  title = "404",
+  body = "The page you’re looking for doesn’t exist.",
 }) {
   return (
-    <section className="mt-28">
-      <div className="container mx-auto min-h-screen">
-        <div className="flex flex-col items-center justify-center">
-          <div className="w-full md:w-4/12 text-center">
-            <h2 className="text-3xl font-semibold mb-6">{title}</h2>
-            <p className="text-lg mb-12">{body}</p>
-            <Link
-              to="/"
-              className="text-gray-900 bg-red-200 focus:outline-none w-full py-3 rounded-full text-lg focus:text-black transition-all duration-200 px-8 cursor-pointer"
-            >
-              Back to Home
-            </Link>
-          </div>
+    <>
+      <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+          <h1 class="display-1 fw-bold">{title}</h1>
+          <p class="fs-3">
+            <span class="text-danger">Opps!</span> Page not found.
+          </p>
+          <p class="lead">{body}</p>
+          <Link to="/" class="btn btn-primary">
+            Go Home
+          </Link>
         </div>
       </div>
-    </section>
+    </>
   );
 }
